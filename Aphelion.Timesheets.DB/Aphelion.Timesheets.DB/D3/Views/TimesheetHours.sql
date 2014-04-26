66039ad0d1fe9b27f2c94fe41197d03fffbc0180
MarkGStacey <mark.stacey@aphelion.ws>
2014-04-26 23:57:57 +0200

    DB changes

--- Aphelion.Timesheets.DB/Aphelion.Timesheets.DB/D3/Views/TimesheetHours.sql
+++ Aphelion.Timesheets.DB/Aphelion.Timesheets.DB/D3/Views/TimesheetHours.sql
@@ -1,13 +1,19 @@
 ﻿
 
-CREATE VIEW D3.TimesheetHours
+CREATE VIEW [D3].[TimesheetHours]
 AS
 
-select Convert(date,Convert(char(10),TD.TimesheetDetailDateID), 111) as c_Date
+select 
+TOP 2147483647 
+
+ISNULL(Convert(date,Convert(char(10),TD.TimesheetDetailDateID), 111), '2000/01/01') as c_Date
 , P.PersonName as Name
 , SUM([Hours]) As Value
 from dbo.TimesheetDetail TD
 INNER JOIN dbo.Person P
 ON TD.TimesheetDetailPersonID = P.PersonID
-GROUP BY Convert(date,Convert(char(10),TD.TimesheetDetailDateID), 111)
+WHERE P.Active = 1
+AND ROUND(td.TimesheetDetailDateID/10000,0) = year(getdate())
+GROUP BY TD.TimesheetDetailDateID
 , P.PersonName
+ORDER BY P.PersonName , td.TimesheetDetailDateID
